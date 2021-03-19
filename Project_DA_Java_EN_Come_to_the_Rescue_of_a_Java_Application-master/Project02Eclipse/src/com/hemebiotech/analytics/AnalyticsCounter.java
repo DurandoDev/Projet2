e974a0bc5623172d195473ed37018e9323c6f4ac
Nathan D <nat.durando@gmail.com>
2021-03-19 16:46:07 +0100

    Suppression de la variable <<headCount>> (inutile) et remplacement par <<headacheCount>> déjà déclarée au début de la classe et modification de la seconde condition du if <<rush>> par <<rash>> (faute de frappe)

--- Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application-master/Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application-master/Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -4,28 +4,31 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 
+
 public class AnalyticsCounter {
 	private static int headacheCount = 0;	// initialize to 0
 	private static int rashCount = 0;		// initialize to 0
 	private static int pupilCount = 0;		// initialize to 0
-	
+
+
 	public static void main(String args[]) throws Exception {
 		// first get input
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
 
-		int headCount = 0;	// counts headaches
 		while (line != null) {
 			System.out.println("symptom from file: " + line);
 			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
+				headacheCount++;
+				System.out.println("number of headaches: " + headacheCount);
 			}
-			else if (line.equals("rush")) {
+			else if (line.equals("rash")) {
 				rashCount++;
+				System.out.println("number of rash: " + rashCount);
 			}
 			else if (line.contains("pupils")) {
 				pupilCount++;
+				System.out.println("number of dialated pupils: " + pupilCount);
 			}
 
 			line = reader.readLine();	// get another symptom
