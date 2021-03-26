611d2376682f0e63fbcb08e9a7bbe7545ab75b96
Nathan Durando <nat.durando@gmail.com>
2021-03-26 17:19:38 +0100

    small fixes

--- Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application-master/Project02Eclipse/src/com/hemebiotech/analytics/SymptomCount.java
+++ Project_DA_Java_EN_Come_to_the_Rescue_of_a_Java_Application-master/Project02Eclipse/src/com/hemebiotech/analytics/SymptomCount.java
@@ -6,15 +6,16 @@ import java.util.Map;
 import java.util.TreeMap;
 
 /**
- * counting and sorting each symptom from a list of symptoms into result.out
+ * counting and sorting each symptom from a list of symptoms into result.out.
  */
 public class SymptomCount {
     /**
-     * Count the symptoms occurrence and place them into result.out
+     * Count the symptoms occurrence and place them into result.out.
+
      * @param source file to read
      * @param destination file to write in
      */
-    public void symptomCount(String source,String destination) throws IOException {
+    public void symptomCount(final String source, final String destination) throws IOException {
 
             String symptom;
             Map<String, Integer> symptomCount = new TreeMap<>();
@@ -22,36 +23,36 @@ public class SymptomCount {
               Initialize the file where we're going to put our result data
               @see FileWriter
              */
-            FileWriter writer = new FileWriter (destination);
+            FileWriter writer = new FileWriter(destination);
             /*
               Create a list from the file specified in parameter
               @see ReadSymptomDataFromFile
              */
             ISymptomReader listSymptom = new ReadSymptomDataFromFile(source);
 
-            for (int i=0;i<listSymptom.GetSymptoms().size();i++){
+            for (int i = 0; i < listSymptom.GetSymptoms().size(); i++) {
 
                 symptom = listSymptom.GetSymptoms().get(i);
 
                 /*
                   Verify if the symptom haven't been already check
                  */
-                if ((symptomCount.containsKey(symptom))){
+                if ((symptomCount.containsKey(symptom))) {
                     /*
                       increment the count for the key associated
                      */
-                    symptomCount.put(symptom,symptomCount.get(symptom)+1);
-                }else {
-                    symptomCount.put(symptom,1);
+                    symptomCount.put(symptom, symptomCount.get(symptom) + 1);
+                } else {
+                    symptomCount.put(symptom, 1);
                 }
             }
             /*
               Write the keys and the values in result.out
              */
-            for (Map.Entry<String,Integer> entry:symptomCount.entrySet()){
+            for (Map.Entry<String, Integer> entry:symptomCount.entrySet()) {
                 String key = entry.getKey();
                 Integer value = entry.getValue();
-                writer.write(key+": "+value+"\n");
+                writer.write(key + ": " + value + "\n");
             }
             writer.close();
     }
